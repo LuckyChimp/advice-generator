c696a412b9dd7b826a0a294fe7db4fb542572083
LuckyChimp <lukas.hartwich00@gmail.com>
2022-05-04 19:25:40 +0200

    First commit of whole project

---
+++ README.md
@@ -0,0 +1,41 @@
+# Frontend Mentor - Advice generator app solution
+
+This is a solution to the [Advice generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Link](#link)
+- [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Generate a new piece of advice by clicking the dice icon
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Link
+
+- Solution URL: [https://luckychimp.github.io/advice-generator/](https://luckychimp.github.io/advice-generator/)
+
+## Built with
+
+- Semantic HTML5 markup
+- SCSS for clean, easy and less CSS
+- Flexbox
+
+## Author
+
+- Frontend Mentor - [@LuckyChimp](https://www.frontendmentor.io/profile/LuckyChimp)
+
---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+window.onload = async () => {
+    // init advice id and text with random advice
+    const randomAdvice = await fetchRandomAdvice();
+    insertInDOM(randomAdvice.id, randomAdvice.advice);
+    
+    // click listener to show new advice
+    let newAdviceBtn = document.getElementById('show-new-advice');
+    newAdviceBtn.addEventListener('click', async () => {
+        const randomAdvice = await fetchRandomAdvice();
+        insertInDOM(randomAdvice.id, randomAdvice.advice);
+    });
+    
+    // fetch new advice via the simple adviceSlip-API
+    async function fetchRandomAdvice() {
+        const adviceApiUrl = 'https://api.adviceslip.com/advice';
+        const res = await fetch(adviceApiUrl, {method: 'GET'});
+        const data = await res.json();
+        return data.slip;
+    }
+
+    // insert new advice id and advice text into DOM
+    function insertInDOM(adviceID, adviceText) {
+        let adviceIdElmnt = document.getElementById('advice-id');
+        let adviceTextElmnt = document.getElementById('advice-text');
+
+        adviceIdElmnt.textContent = adviceID;
+        adviceTextElmnt.textContent = '"'+adviceText+'"';
+    }
+}
